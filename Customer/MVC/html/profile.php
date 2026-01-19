5e85c7f80cc960bf031ae43029c6b0966046bf2b
Rupam Debnath <ropamloves@gmail.com>
2026-01-19 14:17:18 +0600

    feat: navbar done (T-17)

--- Customer/MVC/html/profile.php
+++ Customer/MVC/html/profile.php
@@ -17,3 +17,17 @@ $user = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM users WHERE id = '
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
+
+<body class="checkout-page">
+    <header class="header-main">
+        <div class="header-container">
+            <div class="logo">Krishibazar</div>
+            <nav class="header-nav">
+                <a href="dashboard.php">Home</a>
+                <a href="cart.php">Cart</a>
+                <a href="orders.php">Orders</a>
+                <a href="profile.php" class="active">Me</a>
+                <a href="../php/logout.php" style="color: #d9534f;"><i class="fa-solid fa-right-from-bracket"></i> Logout</a>
+            </nav>
+        </div>
+    </header>
