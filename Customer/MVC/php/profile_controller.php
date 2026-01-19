f741fd1a2d3047bb5ad4a7a8f19e78a7c690ef61
Rupam Debnath <ropamloves@gmail.com>
2026-01-19 14:24:26 +0600

    feat: image logic (T-17)

--- Customer/MVC/php/profile_controller.php
+++ Customer/MVC/php/profile_controller.php
@@ -16,3 +16,13 @@ if (!empty($new_pass)) {
     $update_fields .= ", password = '$new_pass'";
 }
 
+$new_image_name = null;
+if (!empty($_FILES['profile_pic']['name'])) {
+    $new_image_name = time() . '_' . $_FILES['profile_pic']['name'];
+    // Moving files specifically to your 'images' folder
+    if(move_uploaded_file($_FILES['profile_pic']['tmp_name'], "../images/" . $new_image_name)) {
+        $update_fields .= ", profile_image = '$new_image_name'";
+    }
+}
+
+
